e7052972a93f82bcdcd278ea1ee5ee27a274856e
稻草人 <462001808@qq.com>
2017-06-15 15:51:29 +0800

    安装babel

--- staticv3/app/js/index.js
+++ staticv3/app/js/index.js
@@ -1,6 +1,20 @@
-var obj=document.querySelectorAll("ul li");
-console.log(obj.length);
-obj.forEach(item=>{
-  console.log(item);
-  // console.log('2');
-})
+// var obj=document.querySelectorAll("ul li");
+// console.log(obj.length);
+// obj.forEach(item=>{
+//   console.log(item);
+//   // console.log('2');
+// })
+
+
+var asyncFun = async function () {
+  var set=setTimeout(function(){
+      console.log(3);
+  },1000)
+  var f1 = await set;
+  console.log(1);
+  var f2 = await set;
+  console.log(1);
+  var f2 = await set;
+
+};
+asyncFun()
--- staticv3/readMe.md
+++ staticv3/readMe.md
@@ -1,7 +1,7 @@
 # 使用简介
 
 1. 配置了scss，mock,gulp
-2. 配置webpack
+2. 配置webpack,解析ES6,vue文件
 
 ## 执行DEMO
 
