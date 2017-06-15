7ae1c5774ca772510013cb15b88156499c42237b
稻草人 <462001808@qq.com>
2017-06-15 18:46:14 +0800

    初始化完成es6

--- es6解析版/app/js/index.js
+++ es6解析版/app/js/index.js
@@ -1,3 +1,10 @@
-$(document).ready(function() {
-  $('#app div').html('测试')
-});
+// var obj=document.querySelectorAll("ul li");
+// console.log(obj.length);
+// obj.forEach(item => {
+//     console.log(item);
+//     // console.log('2');
+// })
+
+const arr = [1, 2, 3];
+console.log(arr.includes(1));
+[1,2,3].map(n => n + 1);
--- es6解析版/gulpfile.js
+++ es6解析版/gulpfile.js
@@ -8,20 +8,20 @@ var sass=require('gulp-sass');
 // 静态服务器 + 监听 css/html/js文件
 gulp.task('serve',function(){
   browserSync.init({
-    server:'./app'
+    server:'./public'
   });
 
-  gulp.watch('app/scss/*.scss', ['sass']);
-  gulp.watch('app/css/*.css').on('change',reload);
-  gulp.watch('app/js/*.js').on('change',reload);
-  gulp.watch('app/*.html').on('change',reload);
+  gulp.watch('public/scss/*.scss', ['sass']);
+  gulp.watch('public/css/*.css').on('change',reload);
+  gulp.watch('public/js/*.js').on('change',reload);
+  gulp.watch('public/*.html').on('change',reload);
 
 });
 
 gulp.task('sass',function(){
-  return gulp.src('app/scss/*.scss')
+  return gulp.src('public/scss/*.scss')
   .pipe(sass().on('error',sass.logError))
-  .pipe(gulp.dest('./app/css'));
+  .pipe(gulp.dest('./public/css'));
 })
 
 
