56787b8eed59ab6b98a907da1f68e2739633173a
稻草人 <462001808@qq.com>
2017-06-15 18:42:35 +0800

    dd

--- es6解析版/gulpfile.js
+++ es6解析版/gulpfile.js
@@ -8,20 +8,20 @@ var sass=require('gulp-sass');
 // 静态服务器 + 监听 css/html/js文件
 gulp.task('serve',function(){
   browserSync.init({
-    server:'./public'
+    server:'./app'
   });
 
-  gulp.watch('public/scss/*.scss', ['sass']);
-  gulp.watch('public/css/*.css').on('change',reload);
-  gulp.watch('public/js/*.js').on('change',reload);
-  gulp.watch('public/*.html').on('change',reload);
+  gulp.watch('app/scss/*.scss', ['sass']);
+  gulp.watch('app/css/*.css').on('change',reload);
+  gulp.watch('app/js/*.js').on('change',reload);
+  gulp.watch('app/*.html').on('change',reload);
 
 });
 
 gulp.task('sass',function(){
-  return gulp.src('public/scss/*.scss')
+  return gulp.src('app/scss/*.scss')
   .pipe(sass().on('error',sass.logError))
-  .pipe(gulp.dest('./public/css'));
+  .pipe(gulp.dest('./app/css'));
 })
 
 
