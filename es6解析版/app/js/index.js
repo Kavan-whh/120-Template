c1683889614270d05bd3b0af9790c8ee22567879
稻草人 <462001808@qq.com>
2017-06-15 19:33:46 +0800

    完整模板

--- es6解析版/app/js/index.js
+++ es6解析版/app/js/index.js
@@ -4,7 +4,36 @@
 //     console.log(item);
 //     // console.log('2');
 // })
-
+"use strict"
 const arr = [1, 2, 3];
 console.log(arr.includes(1));
 [1,2,3].map(n => n + 1);
+var o={
+  create(){
+    console.log('fun');
+  }
+}
+o.create()
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+    greet() {
+        return this.name + ' says hello. ';
+    }
+}
+
+console.log(new Person("cindy").greet());
+
+var asyncFun = async function() {
+    var set = setTimeout(function() {
+        console.log(3);
+    }, 1000)
+    var f1 = await set;
+    console.log(1);
+    var f2 = await set;
+    console.log(1);
+    var f2 = await set;
+
+};
+asyncFun()
